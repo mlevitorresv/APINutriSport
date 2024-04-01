8191bba380510e8a204d01b0edd331a6c1e3901b
mlevitorresv <levitorres66@gmail.com>
2024-04-01 14:49:53 +0200

    bill model created

--- src/models/Bills.ts
+++ src/models/Bills.ts
@@ -1,1 +1,24 @@
 import Joi from 'joi'
+
+export interface BillsInterface{
+    beneficiary: string
+    description: string
+    type: string
+    paymentAmount: number
+    date: Date
+}
+
+enum BillType{
+    Compra = "compra",
+    Alquiler = "alquiler",
+    Nómina = "nómina"
+}
+
+export const billsSchema = Joi.object({
+    id: Joi.number().integer().positive().required(),
+    beneficiary: Joi.string().required(),
+    description: Joi.string().required(),
+    type: Joi.string().valid(...Object.values(BillType)).required(),
+    paymentAmount: Joi.number().required(),
+    date: Joi.date().required(),
+})
