08602ae483a9eeb290c856478ff128eb02c9a00d
mlevitorresv <levitorres66@gmail.com>
2024-04-02 11:36:54 +0200

    products controller and service created

---
+++ src/controllers/product.ts
@@ -0,0 +1,63 @@
+import express, { Request, Response } from 'express'
+import { ProductInterface } from '../models/Product';
+import { deleteProduct, fetchAllProducts, fetchProductsById, patchProduct, postProduct } from '../services/product';
+
+
+export const productRouter = express.Router();
+
+productRouter.get('/', async (req: Request, res: Response) => {
+    try {
+        const allProducts: ProductInterface[] = await fetchAllProducts();
+        res.json({ products: allProducts })
+    } catch (error) {
+        console.error('Error getting the products: ', error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+
+productRouter.get('/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const product: ProductInterface = await fetchProductsById(id);
+        res.json({ products: product })
+    } catch (error) {
+        console.error('Error getting the product: ', error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+
+productRouter.post('/', async (req: Request, res: Response) => {
+    try {
+        const result = await postProduct(req.body);
+        res.json(result)
+    } catch (error) {
+        console.error('Error saving the product: ', error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+
+productRouter.patch('/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const result = await patchProduct(id, req.body);
+        res.json(result)
+    } catch (error) {
+        console.error('Error updating the product: ', error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+
+productRouter.delete('/:id', async (req: Request, res: Response) => {
+    try {
+        const id: string = req.params.id;
+        const result = await deleteProduct(id);
+        res.json(result)
+    } catch (error) {
+        console.error('Error deleting the product: ', error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
---
+++ src/services/product.ts
@@ -0,0 +1,71 @@
+import { mysqlConnect, executeQuery } from "../config/db";
+import { ProductInterface } from "../models/Product";
+
+export const fetchAllProducts = async (): Promise<any> => {
+    try {
+        const [result, fields] = await executeQuery(`SELECT * FROM products`)
+        return result;
+    } catch (error) {
+        console.error('Error, products were not obtained: ', error)
+        throw error;
+    }
+}
+
+export const fetchProductsById = async (id: string): Promise<any> => {
+    try{
+        const [result, fields] = await executeQuery(`SELECT * FROM productss WHERE id = ${id}`)
+        return result;
+    }catch(error){
+        console.error('Error, product were not obtained: ', error)
+        throw error;
+    }
+}
+
+
+export const postProduct = async(product: ProductInterface) => {
+    try {
+        const query = `
+        INSERT INTO products (PVP, SKU, brand, carbohydrates, category, description, dimensions, energy, fats, ingredients, instructions, name, photos, proteins, salt, stock, weight)
+        VALUES ('${product.PVP}', '${product.SKU}', '${product.brand}', '${product.carbohydrates}', '${product.category}', '${product.description}', '${product.dimensions}', '${product.energy}', '${product.fats}', '${product.ingredients}', '${product.instructions}', '${product.name}', '${product.photos}', '${product.proteins}', '${product.salt}', '${product.stock}', '${product.weight}')
+        `
+
+        const [result, fields] = await executeQuery(query)
+        return { success: true, product: product }
+    } catch (error) {
+        console.error('Error, product not saved: ', error)
+        throw error;
+    }
+}
+
+
+export const patchProduct = async (id: string, body: ProductInterface) => {
+    try {
+        const updateFields = {...body};
+        const keys = Object.keys(updateFields)
+        const values = Object.values(updateFields)
+        
+        const setClause = keys.map(key => `${key} = ?`).join(', ')
+
+        const query = `UPDATE products SET ${setClause} WHERE id = ?;`;
+
+        const updateValues = [...values, id];
+
+        executeQuery(query, updateValues)
+        return { success: true, user: body }
+
+    } catch (error) {
+        console.error('Error, product not updated: ', error)
+        throw error;
+    }
+}
+
+
+export const deleteProduct = async (id: string) => {
+    try {
+        const [result, fields] = await executeQuery(`DELETE FROM products WHERE id = ${id}`)
+        return result;
+    } catch (error) {
+        console.error('Error, product not deleted: ', error)
+        throw error;
+    }
+}
