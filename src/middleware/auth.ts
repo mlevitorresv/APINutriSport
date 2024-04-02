392b9301c3dbcb582546d03bccc0a7551f20ce40
mlevitorresv <levitorres66@gmail.com>
2024-04-02 13:11:42 +0200

    dotenv in auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
 
 interface AuthenticatedRequest extends Request {
     user?: any
