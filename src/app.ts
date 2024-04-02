bc54b1d7fe1571ff2b3592bb0196dfc562bd3770
mlevitorresv <levitorres66@gmail.com>
2024-04-02 11:56:38 +0200

    routes added in app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,37 @@
-import express, {Request, Response} from 'express'
+import express from 'express'
+import { authMiddleware } from './middleware/auth'
+import { loginRouter } from './controllers/login'
+import { mysqlConnect } from './config/db'
+import { billRouter } from './controllers/bill'
+import { commentRouter } from './controllers/comment'
+import { customerRouter } from './controllers/customer'
+import { employeeRouter } from './controllers/employee'
+import { productRouter } from './controllers/product'
+import { saleRouter } from './controllers/sale'
+import { supplierRouter } from './controllers/supplier'
+
 
 export const app = express()
 
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({ res:'Hello World!' })
-})
+mysqlConnect();
+app.use(express.json())
 
+app.use('/login', loginRouter)
+// app.use('/public', publicRouter)
 
+app.use(authMiddleware);
 
+app.use('/bills', billRouter)
+
+app.use('/comments', commentRouter)
+
+app.use('/customers', customerRouter)
+
+app.use('/employees', employeeRouter)
+
+app.use('/products', productRouter)
+
+app.use('/sales', saleRouter)
+
+app.use('/suppliers', supplierRouter)
