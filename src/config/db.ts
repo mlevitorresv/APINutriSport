cea3a4a7f4fd187b4717c67b6545eb707d6c2b9d
mlevitorresv <levitorres66@gmail.com>
2024-04-02 12:31:41 +0200

    IINSTALL dotenv for mysqqlconnection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,6 @@
-import mysql, { ConnectionOptions } from "mysql2/promise";
-
+import mysql, { Pool, PoolOptions } from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db_host = process.env.DB_HOST;
 const db_user = process.env.DB_USER;
@@ -8,20 +9,20 @@ const db_database = process.env.DB_DATABASE
 const db_port = process.env.DB_PORT;
 
 
+const pool: Pool = mysql.createPool({
+    host: db_host,
+    user: db_user,
+    password: db_password,
+    database: db_database,
+    port: 3306
+})
 
-export const mysqlConnect = async () => {
-    const connectionConfig: ConnectionOptions = {
-        host: db_host,
-        user: db_user,
-        password: db_password,
-        database: db_database
-    }
-
-    const connection = mysql.createConnection(connectionConfig)
+export const mysqlConnect = async ()=> {
 
     try {
+        const connection = await pool.getConnection();
         console.log('Success Connection')
-        return connection
+        return connection;
     } catch (e) {
         console.log('DB error:', e)
         throw e
